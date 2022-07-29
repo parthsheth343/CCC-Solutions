6bca6093be72cbbb2ac09aebb68d7310a3439a62
Parth Sheth <parthsheth343@gmail.com>
2022-07-28 23:37:02 -0600

    j1

---
+++ 2010/j1.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int fingers;
+  int total;
+  cin >> fingers;
+  switch ( fingers ) {
+    case 1: {
+        total = 1;
+        break;
+      }
+    case 2: {
+        total = 2;
+        break;
+      }
+    case 3: {
+        total = 2;
+        break;
+      }
+    case 4: {
+        total = 3;
+        break;
+      }
+    case 5: {
+        total = 3;
+        break;
+      }
+    case 6: {
+        total = 3;
+        break;
+      }
+    case 7: {
+        total = 2;
+        break;
+      }
+    case 8: {
+        total = 2;
+        break;
+      }
+    case 9: {
+        total = 1;
+        break;
+      }
+    case 10: {
+        total = 1;
+        break;
+      }
+  }
+
+    
+  
+  cout << total;
+  
+}
