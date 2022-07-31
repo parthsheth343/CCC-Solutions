06bd7e3741bfbe90be6fb78d04a79a063752c074
Parth Sheth <parthsheth343@gmail.com>
2022-07-30 22:29:13 -0600

    j2

--- 2011/j2_2011.cpp
+++ 2011/j2_2011.cpp
@@ -8,9 +8,6 @@ int main() {
     bool reached = false;
     for (int t =1; t<=maxtime; t++) {
         int altitude = -6*pow(t,4) + humid*pow(t,3) + 2*pow(t,2) +t;
-        if (altitude <=0) {cout<<"The balloon first touches ground at hour:"<<endl; cout<<t; reached = true; break;}
-    }
+        if (altitude <=0) {cout<<"The balloon first touches ground at hour:"<<endl; cout<<t; reached = true; break;}}
     if (reached ==false) {cout<<"The balloon does not touch ground in the given time."<<endl;}
-
-
 }
